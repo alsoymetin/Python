bf49d5ca566bda484b755fa1e794dd6555feec6d
alsoymetin <161921035+alsoymetin@users.noreply.github.com>
2024-03-02 12:01:12 +0300

    Grup92 Hafta1 Ders1

---
+++ Metin02032024Vektorel/MetinYeni.py
@@ -0,0 +1,8 @@
+## print komutu/fonksiyonu içinde tek, çift, üç tırnak kullanılablir.
+print ('Merhaba')
+print ("Nasılsın")
+print ('''Nasılsınız?''')
+print ('''Eren'in annesi "gel" dedi''')
+print ('Eren\'in annesi "gel" dedi')
+print ('Şeyma Metin\'in kalbini kırdı')
+print ('Metin çünkü Şeyma\'ya boş yere trip atmıyor, sevdiği insan onu üzünce trip atıyor, o yüzden kalbi kırılıyor.')
