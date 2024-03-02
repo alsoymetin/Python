6f91ed0a115fe412deb3643910848f6b6866a816
alsoymetin <161921035+alsoymetin@users.noreply.github.com>
2024-03-02 13:12:20 +0300

    Add files via upload

---
+++ Metin02032024Vektorel/01_print_farklı_kullanımlar.py
@@ -0,0 +1,21 @@
+## string ve numeric ifadelerin ve değişken ifadelerin Kullanımı
+print ("Mahir","Barış")
+print ("Mahir"+"Barış")
+print ("Atakan"+"35")
+print ("Gökhan "*10)
+
+metin = 34
+seyma = 36
+print (metin+seyma)
+print ("metin","seyma")
+print ("metin"+"seyma")
+print (metin+seyma+10)
+
+
+
+
+
+
+
+
+
---
+++ Metin02032024Vektorel/Hesap makinesi sablon.py
@@ -0,0 +1,10 @@
+##onat = 12
+##print (onat)
+ad = "Metin"
+yas = 12
+print(yas)
+print ("Demek yaşın", yas)
+print (f"{ad} Demek yaşın {yas}")
+
+
+
