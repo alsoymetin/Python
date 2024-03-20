64ba67ce479d5bcaaffd005ffdec381fa342c3fc
alsoymetin <alsoymetin@gmail.com>
2024-03-20 12:12:16 +0300

    Menuguncel

--- Metin16Mart/Uygulamavealtlar/03.03.2024 Python/2.deneme/anamenu.py
+++ Metin16Mart/Uygulamavealtlar/03.03.2024 Python/2.deneme/anamenu.py
@@ -4,6 +4,7 @@ import nothesap.nothesaplama
 import cizimler.cizim
 import hesaplar.sicaklik
 import bilgiler.gezegenler
+import hesaplar.uzunluk
 def anamenu():
     print("╔════════════════════════════════╗")
     print("║     ANAMENU                    ║")
@@ -14,7 +15,8 @@ def anamenu():
     print("║  4-Şekil Çizimi                ║")
     print("║  5-Sıcaklık Dönüştürücü        ║")
     print("║  6-Gezegenler hakkında bilgiler║")
-    print("║  e-Çıkış                       ║")    
+    print("║  7-Uzunluk birimi dönüştürücü  ║")
+    print("║  X-Çıkış                       ║")    
     print("║                                ║")
     print("║    Seçiminiz nedir?            ║")
     print("╚════════════════════════════════╝")
@@ -37,7 +39,10 @@ def anamenu():
     if secim == "6" :
         bilgiler.gezegenler.gmenu ()
         anamenu ()   
-    if secim == "E" or secim == "e" :
+    if secim == "7" : 
+        hesaplar.uzunluk.umenu ()
+        anamenu ()
+    if secim == "X" or secim == "x" :
         exit ()
     else:
         print ("secim sadece 1,2,3,4,e olabilir.")
---
+++ Metin16Mart/Uygulamavealtlar/03.03.2024 Python/2.deneme/hesaplar/uzunluk.py
@@ -0,0 +1,7 @@
+def umenu():
+    def uzunluk(aa):
+        print(f"Santimetre cinsinden uzunluk ölçüsü giriniz:{0.393701*aa}")
+    sayi1 = int(input("Birim giriniz"))
+    uzunluk(sayi1)
+
+input()
